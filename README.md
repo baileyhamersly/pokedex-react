3dcd3978d13a8993aebaf90a6a982f3c043d23d2
Bailey Hamersly <baileyhamersly@gmail.com>
2022-08-08 19:37:35 -0500

    Fixing localhost

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pokedex-react
 Created with CodeSandbox
+  /*
+Instructions
+1. Create a dropdown that displays the names of the first 151 pokemon
+- fetch pokemon list at https://pokeapi.co/api/v2/pokemon?limit=151
+
+2. Render the image for the first pokemon
+- fetch first pokemon information at https://pokeapi.co/api/v2/pokemon/1
+- in the response, find image source in sprites > front_default
+
+3. After a pokemon is selected, render the image for that selected pokemon
+- fetch pokemon information at https://pokeapi.co/api/v2/pokemon/{POKEMON NAME}
+- in the response, find image source in sprites > front_default
+*/
--- src/App.js
+++ src/App.js
@@ -50,7 +50,6 @@ export default function App() {
     }
     getData();
     console.log("pokeList", pokeList);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -62,20 +61,6 @@ export default function App() {
     }
     getData();
   }, [currentPoke]);
-
-  /*
-Instructions
-1. Create a dropdown that displays the names of the first 151 pokemon
-- fetch pokemon list at https://pokeapi.co/api/v2/pokemon?limit=151
-
-2. Render the image for the first pokemon
-- fetch first pokemon information at https://pokeapi.co/api/v2/pokemon/1
-- in the response, find image source in sprites > front_default
-
-3. After a pokemon is selected, render the image for that selected pokemon
-- fetch pokemon information at https://pokeapi.co/api/v2/pokemon/{POKEMON NAME}
-- in the response, find image source in sprites > front_default
-*/
 
   return (
     <div className="App">
@@ -104,7 +89,6 @@ Instructions
         src={currentPokeData?.pokeData?.sprites?.back_default}
         alt={currentPokeData?.pokeData?.name}
       ></img>
-      <div>{/* {currentPokeData.pokeDesc.} */}</div>
     </div>
   );
 }
